8fa87d4b5ff7989aedb57196c42363b5865fc5a7
Chanakya-dev <Chanakyamanas123@gmail.com>
2025-04-26 13:21:12 +0530

    Loops

---
+++ index.py
@@ -0,0 +1,20 @@
+# Loops
+# for loop ,while loop and for each loop
+print("Chinna")
+print("Chinna")
+print("Chinna")
+print("Chinna")
+
+print("for loop")
+for i in range(1,11,3):
+    print(i)
+
+
+i=1
+while i<=20:
+    if i%2==0:
+        print(f"{i} is even number")
+    i=i+1
+
+# 123
+# 321
